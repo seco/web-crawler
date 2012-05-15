7d4e5d9363e994c3da58bed8254c8f07ee277689
ibrahim.guenduez <ibrahim.gunduz@rocket-internet.com.tr>
2012-05-15 15:16:25 +0300

    Added or updated some method comments.

--- library/Crawler.php
+++ library/Crawler.php
@@ -1,11 +1,19 @@
 <?php
 class Crawler
 {
+  /**
+  * returns new object instance.
+  * @return Crawler
+  */
   public function getInstance()
   {
     return new self;
   }
 
+  /**
+  * execute crawle jobs
+  * @param array $jobs
+  */
   public static function run($jobs)
   {
     $crawler = self::getInstance();
@@ -20,17 +28,31 @@ class Crawler
     }
   }
 
+  /**
+  * returns crawler adapter
+  * @param string $crawler
+  * @return Crawler_Interface
+  */
   private function getAdapter($crawler)
   {
     $factory = new Crawler_Factory();
     return $factory->create($crawler);
   }
 
+  /**
+  * writes crawled data to log.
+  * @param array $data
+  */
   private function logData($data)
   {
-    $logText = implode("\t", $data) . PHP_EOL;
-    $customLogText = implode("\t", array($data[0], $data[1], $data['price'], $data['currency']) ) . PHP_EOL;
+    $logText        = implode("\t", $data) . PHP_EOL;
+    $customLogText  = implode("\t", array($data[0], $data[1], $data['price'], $data['currency']) ) . PHP_EOL;
+
     echo $customLogText;
-    file_put_contents(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'data.log', $logText, FILE_APPEND);
+
+    file_put_contents(APPLICATION_PATH . DIRECTORY_SEPARATOR . 
+        'data' . DIRECTORY_SEPARATOR . 
+        'data.log', $logText, 
+        FILE_APPEND);
   }
 }
